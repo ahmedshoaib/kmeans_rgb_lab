d045711456797d8a07673f03013bf31a38d27a32
Shoaib <ahmed.shoaib1212@gmail.com>
2019-02-16 19:45:35 -0100

    Adds optional features to input data

--- kmeans_rgb_var.py
+++ kmeans_rgb_var.py
@@ -2,8 +2,8 @@ import numpy as np
 import cv2
 import sys
 
-def variance_calc(img, w):                             #function to calculate variance values for each pixel location
-	varianceMatrix = np.zeros(img.shape,np.uint8)
+def std_calc(img, w):                             #function to calculate std values for each pixel location
+	stdMatrix = np.zeros(img.shape,np.uint8)
 	ny = len(img)
 	nx = len(img[0])
 
@@ -12,69 +12,98 @@ def variance_calc(img, w):                             #function to calculate va
 	    for j in range(w,ny-w):
 
        		sampleframe = img[j-w:j+w, i-w:i+w]
-       		variance    = np.var(sampleframe)
-       		varianceMatrix[j][i] = int(variance)
-	return varianceMatrix 
+       		std    = np.std(sampleframe)
+       		stdMatrix[j][i] = int(std)
+	return stdMatrix 
 
-file = sys.argv[1]
-K = int(sys.argv[2])
-w = int(sys.argv[3])  #dimension of filter for variance
+if len(sys.argv) < 5:
+	print("Error: Insufficient arguments, program takes four additional arguments : \n 1. input image name\n 2. K\n 3. W : dimension of filter for std dev\n 4. option for feature selection(X,Y,std/texture) : \n|X  |Y  |V  |\n|0/1|0/1|0/1|\n Eg : python kmeans_rgb_var.py test2.jpeg 5 3 101")
+	sys.exit()
+else:
+	file = sys.argv[1]
+	K = int(sys.argv[2])
+	if K < 3:
+		print("Error: K has to be greater than 2")
+		sys.exit()
+	w = int(sys.argv[3])  #dimension of filter for std dev
+	if w < 1:
+		print("Error: w has to be greater than 0")
+		sys.exit()
+	option = sys.argv[4]
+	if(len(option) != 3):
+		print("Error:Std option has to be of length 3")
+		sys.exit()
+
+
 img = cv2.imread(file) #read image
 Z = np.float32(img)  
 flag = 0
-varianceMatrix = variance_calc(Z,w)   #calculate variance values
-flat_var = np.zeros((img.shape[0]*img.shape[1],1))
+stdMatrix = std_calc(Z,w)   #calculate std dev values
+flat_std = np.zeros((img.shape[0]*img.shape[1],1))
+flat_x = np.zeros((img.shape[0]*img.shape[1],1))
+flat_y = np.zeros((img.shape[0]*img.shape[1],1))
 
-for i in range(varianceMatrix.shape[0]):                   #flatten the variance matrix. It has the same shape as image. but variance values for r,g,b are same
-	for j in range(varianceMatrix.shape[1]):
-		flat_var[flag] = varianceMatrix[i][j][0]
+for i in range(stdMatrix.shape[0]):                   #flatten the std dev matrix. It has the same shape as image. but std dev values for r,g,b are same
+	for j in range(stdMatrix.shape[1]):
+		flat_std[flag] = [stdMatrix[i][j][0]]
 		flag += 1
-#print(flat_var.shape)
 
 
-'''  shows that all three values of Variance matrix corresponding to a pixel location are the same
-print(varianceMatrix.shape)
-print(varianceMatrix[100][32].shape,varianceMatrix[100][32])
+flag=0
+for i in range(stdMatrix.shape[0]):                   #flatten the x-values matrix. It has the same shape as image. but std dev values for r,g,b are same
+	for j in range(stdMatrix.shape[1]):
+		flat_x[flag] = [i]
+		flag += 1
+
+
+flag = 0
+for i in range(stdMatrix.shape[0]):                   #flatten the y-values matrix. It has the same shape as image. but std dev values for r,g,b are same
+	for j in range(stdMatrix.shape[1]):
+		flat_y[flag] = [j]
+		flag += 1
+
+
+
+
+'''  shows that all three values of std dev matrix corresponding to a pixel location are the same
+print(stdMatrix.shape)
+print(stdMatrix[100][32].shape,stdMatrix[100][32])
 print("Z : 1 : ",Z.shape)
-for i in range(varianceMatrix.shape[0]):
-	for j in range(varianceMatrix.shape[1]):
-		if varianceMatrix[i][j][0] != varianceMatrix[i][j][1] != varianceMatrix[i][j][2]:
-			print(varianceMatrix[i][j],i,j)
+for i in range(stdMatrix.shape[0]):
+	for j in range(stdMatrix.shape[1]):
+		if stdMatrix[i][j][0] != stdMatrix[i][j][1] != stdMatrix[i][j][2]:
+			print(stdMatrix[i][j],i,j)
 			flag = 1
 
 if flag == 1:
 	print("instance")
 '''
-Z2 = Z.reshape((-1,3))
-#print("Z : 2 : ",Z2.shape)
 
 
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-ret,label,center=cv2.kmeans(Z2,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
-# Now convert back into uint8, and make original image
-center = np.uint8(center)
-res = center[label.flatten()]
-res2 = res.reshape((img.shape))
-
-cv2.imwrite(sys.argv[0]+"out(rgb)_"+str(K)+"_"+file, res2)
+Z2 = Z.reshape((-1,3))  #flattens the rgb features dim : (h*w,3)
 
 
-
-Z3 = np.concatenate((Z2,np.float32(flat_var)),axis = 1)  #include corresponding variance values in input data
-#print(Z3.shape)
-#print(Z3[0],Z3[1])
+# concat x/y/standard deviaiton features as requested by user to the rgb feature array
+if(option[0] == '1'):
+	Z2 = np.concatenate((Z2,np.float32(flat_x)),axis = 1)
+if(option[1] == '1'):
+	Z2 = np.concatenate((Z2,np.float32(flat_y)),axis = 1)
+if(option[2] == '1'):
+	Z2 = np.concatenate((Z2,np.float32(flat_std)),axis = 1)
 
 
 
 
 # define criteria, number of clusters(K) and apply kmeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-ret,label,center=cv2.kmeans(Z3,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+print("Calculting Centers ......\n")
+ret,label,center=cv2.kmeans(Z2,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 
+print("Center matrix : \n",center)
 # Now convert back into uint8, and make original image
-center = np.uint8(np.hsplit(center,np.array([3,4]))[0])   #remove variance values from data
+center = np.uint8(np.hsplit(center,np.array([3,4]))[0])   #remove x/y/std values from data
 
 #print("Centers\n : ",center)
 
@@ -82,8 +111,13 @@ res = center[label.flatten()]
 res2 = res.reshape((img.shape))
 
 
-cv2.imwrite(sys.argv[0]+"out(rgb+var)_"+str(K)+"_"+file, res2)
+cv2.imwrite(sys.argv[0]+"out(rgb)_"+str(K)+"_"+file, res2)
 
+
+
+##################################################################################################
+########################################################################################################
+################################################################################################
 # Comparing with Lab color space
 
 
@@ -96,13 +130,13 @@ Z3 = Z2.reshape((-1,3))
 
 # define criteria, number of clusters(K) and apply kmeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+print("Calculting Centers for Lab space ......\n")
 ret,label,center=cv2.kmeans(Z3,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)  #distance function for CIE Lab : E76 similar to euclidean distance
-#print(type(ret),label.shape,center.shape)
-#print("ret : ",ret)
-#print(center)
 
+
+print("Centers in Lab space : \n",center)
 center2 = (cv2.cvtColor(center.reshape((K,1,3)),56))*255.0  #convert center coordinates to rgb
-
+print("Centers in rgb space based on lab k-means : \n",center2)
 # Now convert back into uint8, and make original image
 center3 = np.uint8(center2)
 res = center3[label.flatten()]
--- readme.txt
+++ readme.txt
@@ -4,8 +4,17 @@ OpenCV-python
 numpy
 
 This code contains Kmeans implementation using OpenCV and numpy. We use two color spaces : RGB and CIE L*a*b*.
-In the RGB space implementation,We first implement the kmeans algorithm in rgb space, then we add a variance feture to the input rgb features and remove it from the centers obtained from the algorithm. Distance is L2 norm.
+In the RGB space implementation,We  implement the kmeans algorithm in rgb space and add an optional feature of adding the X locations, Y locations or standard deviation values corresponding to each pixel location to the feature vector for k-means processing. We later strip the additional feature columns to show the resultant image from just the rgb values. Distance is L2 norm.
+
 In Lab, we first normalize the input RGB(BGR in cv2) by dividing the rgb values by 255 and then converting to LAB space using cv2.cvtColor() function. The distance funciton used here is CIE delta(76) which is L2 norm distance and hence the algorithm can use the kmeans implementation given by cv2. 
 We use Lab space as Lab space was developed to be more similar to our visual experience.
 
-example command : python kmeans_rgb_var.py test2.jpeg 5 3
+example command : python kmeans_rgb_var.py test2.jpeg 7 3 001
+
+program takes four additional arguments : 
+ 1. input image name 
+ 2. K
+ 3. W : dimension of filter for std dev
+ 4. option for feature selection(X,Y,std. deviation/texture) : 
+ 	|X  |Y  |S  |
+ 	|0/1|0/1|0/1|
